019c535e710f0e6807fbedc4e61edce2496ec82c
seo4867 <seo4867@gmail.com>
2026-05-18 16:13:11 +0900

    Add files via upload

---
+++ public/public/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'bitkkot-v1';
+const CORE  = ['/', '/index.html', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+      }
+      return res;
+    }).catch(() =>
+      caches.match(e.request).then(c => c || caches.match('/'))
+    )
+  );
+});
